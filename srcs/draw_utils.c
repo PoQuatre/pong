82db1ff04d5c8d007d50f750277c70dce68b4dd2
PoQuatre <leflem.mateo2002@gmail.com>
2024-09-18 21:56:07 +0200

    style: use hexadecimal for the escape character

--- srcs/draw_utils.c
+++ srcs/draw_utils.c
@@ -6,7 +6,7 @@
 /*   By: mle-flem <mle-flem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 15:53:23 by mle-flem          #+#    #+#             */
-/*   Updated: 2024/09/18 11:29:40 by mle-flem         ###   ########.fr       */
+/*   Updated: 2024/09/18 12:23:20 by mle-flem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ void	draw_separator(t_term *term)
 
 	i = 0;
 	ansi[0] = '\0';
-	ft_strcat(ansi, "[0;");
+	ft_strcat(ansi, "\x1b[0;");
 	ft_itoa(term->cols / 2, &ansi[ft_strlen(ansi)]);
 	ft_strcat(ansi, "H");
 	ft_putstr(ansi);
 	while (i++ < term->rows)
-		ft_putstr("[90m╏[0m[B[D");
+		ft_putstr("\x1b[90m╏\x1b[0m\x1b[B\x1b[D");
 }
 
 void	draw_ball(t_game_state *state)
@@ -34,10 +34,10 @@ void	draw_ball(t_game_state *state)
 	char	ansi[32];
 
 	ansi[0] = '\0';
-	ft_strcat(ansi, "[");
+	ft_strcat(ansi, "\x1b[");
 	ft_itoa(state->ball_pos.y, &ansi[ft_strlen(ansi)]);
 	ft_strcat(ansi, ";");
 	ft_itoa(state->ball_pos.x, &ansi[ft_strlen(ansi)]);
-	ft_strcat(ansi, "H[37m██[0m");
+	ft_strcat(ansi, "H\x1b[37m██\x1b[0m");
 	ft_putstr(ansi);
 }
